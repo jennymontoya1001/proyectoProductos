39e57e423e5ac4888bf597c22488bb32725a1f21
Jenny Montoya <jennyjj1001@gmail.com>
2022-08-07 20:45:35 -0500

    up

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { request, response } = require('express');
 const express = require('express');
+const  path  = require('path');
 const app = express();
 
 app.use(express.json());
@@ -10,4 +12,9 @@ app.use(express.urlencoded({extended: false}));
 app.listen(3001, () => {
   console.log('server started');
 });
+
+//Routes
+app.get('/', (request,response) => {
+  response.sendFile(path.resolve(__dirname,'index.html'))
+})
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,4 +3,23 @@ const router = Router();
 
 const mysqlConnection = require('./db/db');
 
+//Cliente
+//SERVICE POST -- CREACIÓN
+router.post('/Customer_New', (req, res) => {
+    const {ID,NOMBRE,APELLIDO} = req.body
+
+    let customer = [ID,NOMBRE,APELLIDO];
+
+    let customerNew = `
+INSERT INTO CLIENTE(ID,NOMBRE,APELLIDO)
+VALUES (?,?);`
+
+   mysqlConnection.query(customerNew,customer, (err,results,fields) => {
+     if(err){
+       return console.error(err.message);
+     }
+     res.json({message:`Tipo Usuario Ingresado`})
+   });
+  });
+
 module.exports = router;
